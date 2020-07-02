99e49eb0ad7c174b83e3cc46b340ccf1d21be274
NarvekarS <sanjay.narvekar@gmail.com>
2020-07-01 22:43:43 -0500

    Add whitelisted IP to ADW

--- adw-oac/variables.tf
+++ adw-oac/variables.tf
@@ -43,7 +43,7 @@ variable "ad_name" {
 
 
 variable "autonomous_database_cpu_core_count" {
-  default = "4"
+  default = "1"
 }
 
 
@@ -56,6 +56,10 @@ variable "autonomous_database_db_name" {
 
 variable "autonomous_database_display_name" {
   default = "My ATP DB"
+}
+
+variable "autonomous_database_db_version" {
+  default = "19c"
 }
 
 variable "autonomous_database_is_auto_scaling_enabled" {
@@ -76,6 +80,10 @@ variable "autonomous_database_license_model" {
 
 variable "autonomous_database_data_safe_status" {
   default = "NOT_REGISTERED"
+}
+
+variable "autonomous_database_whitelisted_ips" {
+  default = ["240.0.0.0/4"]
 }
 
 variable "analytics_instance_capacity_capacity_type" {}
