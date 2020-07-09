1c39d9d08df3c64a690e58642db7ba20b54e8697
Sanjay Narvekar <Sanjay.Narvekar@gmail.com>
2020-07-09 14:57:11 -0500

    Added adw-odi-data-science folder

---
+++ adw-odi-data-science/stream.tf
@@ -0,0 +1,11 @@
+## Copyright © 2020, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+resource "oci_streaming_stream" "test_stream" {
+    name = var.stream_name
+    partitions = var.stream_partitions
+
+ #   compartment_id = var.compartment_ocid
+    retention_in_hours = var.stream_retention_in_hours
+    stream_pool_id = oci_streaming_stream_pool.test_stream_pool.id
+}
