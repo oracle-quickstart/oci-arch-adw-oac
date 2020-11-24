ffe7be290dfadaf4dbf3bea4994be3b8bf6d3c3b
lfeldman <lukasz.feldman@oracle.com>
2020-11-24 16:16:16 +0100

    Fixed whitelisted ips

--- adw-odi-oac/adb.tf
+++ adw-odi-oac/adb.tf
@@ -9,10 +9,10 @@ resource "oci_database_autonomous_database" "test_autonomous_database" {
     data_storage_size_in_tbs = var.autonomous_database_data_storage_size_in_tbs
     db_name                  = var.autonomous_database_db_name
     db_version               = var.autonomous_database_db_version
- #   data_safe_status         = var.autonomous_database_data_safe_status
+    data_safe_status         = var.autonomous_database_data_safe_status
     db_workload              = "DW"
     display_name             = var.autonomous_database_display_name
     is_auto_scaling_enabled  = var.autonomous_database_is_auto_scaling_enabled
     license_model            = var.autonomous_database_license_model
-#    whitelisted_ips          = [var.autonomous_database_whitelisted_ips, ]
+    whitelisted_ips          = var.autonomous_database_whitelisted_ips
 }
