6112fea895b853e44f831a15df18af9c6be7015b
lfeldman <lukasz.feldman@oracle.com>
2020-11-24 16:26:02 +0100

    Fixed whitelisted ips v2

--- adw-odi-oac/outputs.tf
+++ adw-odi-oac/outputs.tf
@@ -2,23 +2,23 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 output "odi_node_id" {
-  value = "${module.odi.node_id}"
+  value = module.odi.node_id
 }
  
 output "odi_node_public_ip" {
-  value = "${module.odi.node_public_ip}" 
+  value = module.odi.node_public_ip 
 }
 
 output "odi_node_private_ip" {
-  value = "${module.odi.node_private_ip}"
+  value = module.odi.node_private_ip
 }
 
 output "bastion_host_id" {
-  value = "${module.bastion.id}"
+  value = module.bastion.id
 }
 
 output "bastion_host_public_ip" {
-  value = "${module.bastion.public_ip}"
+  value = module.bastion.public_ip
 }
 
 
