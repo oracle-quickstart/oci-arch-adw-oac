efc823e56fd43008753b3473974a7e22ec189043
Carl-Lejerskar <36647860+Carl-Lejerskar@users.noreply.github.com>
2021-08-16 09:43:46 -0400

    Fixing schema.yaml
    Fixing location of new variables for the service connector hub

--- odi-data-science/stream.tf
+++ odi-data-science/stream.tf
@@ -5,8 +5,6 @@ resource "oci_streaming_stream" "test_stream" {
     depends_on = [oci_identity_policy.ODIDataSciencePolicies]
     name = var.stream_name
     partitions = var.stream_partitions
-
- #   compartment_id = var.compartment_ocid
     retention_in_hours = var.stream_retention_in_hours
     stream_pool_id = oci_streaming_stream_pool.test_stream_pool.id
 }
--- odi-data-science/variables.tf
+++ odi-data-science/variables.tf
@@ -3,12 +3,12 @@
 
 # Variables
 
-variable "region" {}
 variable "tenancy_ocid" {}
-variable "compartment_ocid" {}
 #variable "user_ocid" {}
 #variable "fingerprint" {}
 #variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
 
 #Object Storage Bucket Variables
 
